36717298e55fc5769c0361cf17877a71cd6be4ed
Kiphea <djsmyth05@gmail.com>
2025-11-08 17:56:34 +0000

    Task 3 Fixing Mistake (Now Complete)

---
+++ MyCalculatorTests/CalculatorTests.cs
@@ -0,0 +1,13 @@
+﻿using FourthPractical_MyTestLibrary;
+
+namespace MyCalculatorTests
+{
+    [TestClass]
+    public sealed class CalculatorTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
